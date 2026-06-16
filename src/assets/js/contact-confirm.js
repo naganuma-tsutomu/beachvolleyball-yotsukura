c115b692915ee3bd91b7c6aa5c275026473d18db
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2026-06-16 15:38:59 +0900

    fix: delete console

--- src/assets/js/contact-confirm.js
+++ src/assets/js/contact-confirm.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const member05 = document.querySelector('.confirm-member-05');
   const member06 = document.querySelector('.confirm-member-06');
-console.log(member05);
   if (member05) {
     const add05 = member05.querySelector('.confirm-add')?.textContent.trim();
     if (add05 !== 'する') {
